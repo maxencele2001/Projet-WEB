41fe2c7d4dccd5bfc7793a8db18aef0a898af56d
ebbane <ebbanediet@gmailcom>
2020-04-23 18:53:59 +0200

    récupération

--- functions/annonce.php
+++ functions/annonce.php
@@ -1,10 +1,26 @@
-<?php
-require_once __DIR__ . "/db.php";
+<?php 
+require_once __DIR__ . '/db.php';
 
-function getAnnonces(): array
+function getListe():array{
+    $pdo=getPdo();
+    $query = 'SELECT * FROM annonces';
+    $stmt = $pdo->prepare($query);
+    $stmt = $pdo->query($query);
+    return $stmt -> fetchAll(PDO::FETCH_ASSOC);
+}
+
+function getAnnonce(int $id): ?array
 {
   $pdo = getPdo();
+  $query = "SELECT * FROM annonces WHERE id = :id";
+  $stmt = $pdo->prepare($query);
+  $stmt->execute(['id' => $id]);
 
-  $stmt = $pdo->query("SELECT * FROM annonces WHERE visible=1");
-  return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  if (!$row) {
+    return null;
+  }
+
+  return $row;
 }
--- public/logged/annonce.php
+++ public/logged/annonce.php
@@ -69,10 +69,12 @@ function addAnnonce(string $titre,string $adresse, int $nb_chambre, int $nb_voya
   ]);
 }
 
+
 if (!empty($_POST['titre'])){
 echo "jsuis con";
 var_dump($_POST);
 }
+
 // Fichiers multiples
 if(isset($_POST['titre']) && isset($_POST['adresse']) && isset($_POST['nb_chambre']) && isset($_POST['nb_voyageurs']) && isset($_POST['description']) && isset($_POST['prix']) && !empty($_POST['titre']) && !empty($_POST['adresse']) && !empty($_POST['nb_chambre']) && !empty($_POST['nb_voyageurs']) && !empty($_POST['description']) && !empty($_POST['prix'])){
     echo "oui";
